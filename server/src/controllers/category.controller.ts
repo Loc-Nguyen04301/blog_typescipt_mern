fea635edb69d769c4e784aab24710a0bdd65b9f8
loc <nguyengialoc7@gmail.com>
2023-03-12 00:17:15 +0700

    category for backend

---
+++ server/src/controllers/category.controller.ts
@@ -0,0 +1,71 @@
+import { IReqAuth } from "./../config/interface";
+import { Request, Response } from "express";
+import Categories from "../models/category.model";
+
+const categoryController = {
+  getCategories: async (req: IReqAuth, res: Response) => {
+    try {
+      const categories = await Categories.find().sort("-createdAt");
+      return res.json({ categories });
+    } catch (err: any) {
+      return res.status(500).json({ message: err.message });
+    }
+  },
+  createCategory: async (req: IReqAuth, res: Response) => {
+    if (!req.user)
+      return res.status(400).json({ message: "Invalid Authentication." });
+
+    if (req.user.role !== "admin")
+      return res.status(400).json({ message: "Invalid Authentication." });
+    try {
+      const name = req.body.name.toLowerCase();
+      const newCategory = new Categories({ name });
+      await newCategory.save();
+
+      return res.json({ newCategory });
+    } catch (err: any) {
+      let errorMessage;
+      if (err.code === 11000) {
+        errorMessage = err.keyValue.name + " already exists.";
+        console.log(errorMessage);
+      }
+      return res.status(500).json({ message: errorMessage });
+    }
+  },
+  updateCategory: async (req: IReqAuth, res: Response) => {
+    if (!req.user)
+      return res.status(400).json({ message: "Invalid Authentication." });
+
+    if (req.user.role !== "admin")
+      return res.status(400).json({ message: "Invalid Authentication." });
+
+    try {
+      await Categories.findOneAndUpdate(
+        {
+          _id: req.params.id,
+        },
+        { name: req.body.name }
+      );
+
+      res.json({ message: "Update Success!" });
+    } catch (err: any) {
+      return res.status(500).json({ message: err.message });
+    }
+  },
+  deleteCategory: async (req: IReqAuth, res: Response) => {
+    if (!req.user)
+      return res.status(400).json({ message: "Invalid Authentication." });
+
+    if (req.user.role !== "admin")
+      return res.status(400).json({ message: "Invalid Authentication." });
+
+    try {
+      const category = await Categories.findByIdAndDelete(req.params.id);
+      return res.json({ message: "Delete Success!" });
+    } catch (err: any) {
+      return res.status(500).json({ message: err.message });
+    }
+  },
+};
+
+export default categoryController;
--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -1,9 +1,11 @@
 import { Request, Response, Application } from "express";
 import authRoute from "./auth.route";
 import userRoute from "./user.route";
+import categoryRoute from "./category.route";
 const initRoutes = (app: Application) => {
   app.use("/api/v1/auth", authRoute);
   app.use("/api/v1/user", userRoute);
+  app.use("/api/v1/category", categoryRoute);
   return app.use("/", (req: Request, res: Response) => {
     res.send("server on...");
   });
